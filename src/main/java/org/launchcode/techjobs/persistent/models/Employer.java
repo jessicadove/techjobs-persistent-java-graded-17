d898dfd43f196576e3d5b9480c1a29d758f65f2c
Jessica <jlknobeloch@gmail.com>
2023-11-27 22:58:29 -0600

    added annotations, constructors, getters/setters to Employer model.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Location is required.")
+    @Size(min = 2, max = 30, message = "Location name must be 2-30 characters long.")
+    private String location;
+
+    public Employer() {}
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
